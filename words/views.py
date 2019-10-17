a4384ffa42a5a9659f76a96450e6357fdc7e7189
Abderrahman <abderrahman.adardour@rijksoverheid.nl>
2019-10-17 18:30:39 +0200

    specific

--- words/views.py
+++ words/views.py
@@ -14,53 +14,9 @@ def index(request, text):
 
 
 
-def calculate(text):
+# def calculate(text):
     
 
-    Text = text
-
-    for char in '-.,\n':
-        Text=Text.replace(char,' ')
-        Text = Text.lower()
-    
-    word_list = Text.split()
-
-    # Initializing Dictionary
-    d = {}
-
-
-    for word in word_list:
-        d[word] = d.get(word, 0) + 1
-    
-    word_freq = []
-    for key, value in d.items():
-        word_freq.append((value, key))
-    
-    word_freq.sort(reverse=True) 
-
-    num_fre =[]
-    new_array = []
-    for i, value in enumerate(word_freq[:12]):
-        if word_freq[i][0] == word_freq[i+1][0]:
-            # print(value)
-            # print(word_freq[i][0],word_freq[i+1][0])
-            num_fre.append(value)
-            print(num_fre)
-           
-              
-        # print(num_fre)    
-                # print(sorted(num_fre, key=lambda  val :val[1]))
-            
-            # print(value,i)
-   
-    
-    return word_freq[:12]
-
-
-
-# def calculateSpecificWord(text, word):
-    
-#     InputWord = word
 #     Text = text
 
 #     for char in '-.,\n':
@@ -73,15 +29,59 @@ def calculate(text):
 #     d = {}
 
 
-#     for w in word_list:
-#         d[w] = d.get(w, 0) + 1
+#     for word in word_list:
+#         d[word] = d.get(word, 0) + 1
+    
+#     word_freq = []
+#     for key, value in d.items():
+#         word_freq.append((value, key))
+    
+#     word_freq.sort(reverse=True) 
+
+#     num_fre =[]
+#     new_array = []
+#     for i, value in enumerate(word_freq[:12]):
+#         if word_freq[i][0] == word_freq[i+1][0]:
+#             # print(value)
+#             # print(word_freq[i][0],word_freq[i+1][0])
+#             num_fre.append(value)
+#             print(num_fre)
+           
+              
+#         # print(num_fre)    
+#                 # print(sorted(num_fre, key=lambda  val :val[1]))
+            
+#             # print(value,i)
+   
+    
+#     return word_freq[:12]
+
+
+
+def calculateSpecificWord(text, word):
+    
+    InputWord = word
+    Text = text
+
+    for char in '-.,\n':
+        Text=Text.replace(char,' ')
+        Text = Text.lower()
+    
+    word_list = Text.split()
+
+    # Initializing Dictionary
+    d = {}
+
+
+    for w in word_list:
+        d[w] = d.get(w, 0) + 1
 
     
 
-#     if InputWord in d.keys():
-#         return d[InputWord]
-#     else:
-#          return None
+    if InputWord in d.keys():
+        return d[InputWord]
+    else:
+         return None
 
     
 
@@ -94,8 +94,8 @@ def calculateHighestFrequency(request):
         bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation.  We hold these truths to be
         self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.--That to secure these rights, Governments are instituted among Men, deriving their just powers from the consent of the governed, --That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. """
     
-    Word = "connected"
-    count = calculate(Text) 
+    Word = "these"
+    count = calculateSpecificWord(Text, Word) 
 
     # print(word)
     
@@ -135,6 +135,6 @@ def calculateHighestFrequency(request):
 
     
 
-    return HttpResponse("the most frequent word is:  " + str(count))
+    return HttpResponse("het resultaat is:  " + str(count))
     
 
